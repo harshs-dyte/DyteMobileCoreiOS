4d37c5fda7f7f6eb64f88b44c8998271aefde2c5
dyte-devel <76557270+dyte-devel@users.noreply.github.com>
2023-11-21 21:33:17 +0000

    chore(release): update SPM definition

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://dyte-assets.s3.ap-south-1.amazonaws.com/sdk/ios_core/DyteiOSCore-1.25.1-8993bcab-ed08-4200-afdc-8f4219bce686.xcframework.zip"
+let remoteKotlinChecksum = "44ca438a35532dc90203cc612db407ba2dd08717e0777bc84d91e60c88f4b8cb"
+let packageName = "DyteiOSCore"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: "DyteMobileCoreiOS",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: packageName, targets: [packageName, "WebRTC"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/webrtc-sdk/Specs/releases/download/114.5735.02/WebRTC.xcframework.zip",
+            checksum: "c30668eac8f14a21cfb5612ca4e81b10e2d8d8d5e5231c00874ca118eff9a1c0"
+        ),
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        ),
+    ]
+)
+
